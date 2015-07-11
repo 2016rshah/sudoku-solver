0db88e1e9e7ead4c0dd15e6e231cf509c83e3b35
Rushi Shah <2016rshah@gmail.com>
2015-07-10 23:04:17 -0400

    Added initial sudoku code

---
+++ src/Sudoku.hs
@@ -0,0 +1,85 @@
+module Main where
+
+import Data.Char
+
+main = print "Welcome to the sudoku solver!"
+
+data Sudoku = Sudoku [[Maybe Int]]
+            deriving Show
+
+rows :: Sudoku -> [[Maybe Int]]
+rows (Sudoku rs) = rs
+
+example :: Sudoku
+example =
+  Sudoku
+  [ [Just 3, Just 6, Nothing,Nothing,Just 7, Just 1, Just 2, Nothing,Nothing]
+  , [Nothing,Just 5, Nothing,Nothing,Nothing,Nothing,Just 1, Just 8, Nothing]
+  , [Nothing,Nothing,Just 9, Just 2, Nothing,Just 4, Just 7, Nothing,Nothing]
+  , [Nothing,Nothing,Nothing,Nothing,Just 1, Just 3, Nothing,Just 2, Just 8]
+  , [Just 4, Nothing,Nothing,Just 5, Nothing,Just 2, Nothing,Nothing,Just 9]
+  , [Just 2, Just 7, Nothing,Just 4, Just 6, Nothing,Nothing,Nothing,Nothing]
+  , [Nothing,Nothing,Just 5, Just 3, Nothing,Just 8, Just 9, Nothing,Nothing]
+  , [Nothing,Just 8, Just 3, Nothing,Nothing,Nothing,Nothing,Just 6, Nothing]
+  , [Nothing,Nothing,Just 7, Just 6, Just 9, Nothing,Nothing,Just 4, Just 3]
+  ]
+
+--A1
+allBlankSudoku :: Sudoku
+allBlankSudoku = Sudoku (take 9 (repeat row))
+  where row = take 9 (repeat Nothing)
+
+--A2
+isSudoku :: Sudoku -> Bool
+isSudoku (Sudoku rs) = correctLen rs && lenColsWorks rs && digitsInRange rs
+  where
+    allTrue :: [Bool] -> Bool
+    allTrue = foldl (\acc curr -> acc && curr) True
+
+    correctLen :: [a] -> Bool
+    correctLen rs = (length rs) == 9
+
+    lenColsWorks :: [[a]] -> Bool
+    lenColsWorks = allTrue . map correctLen
+
+    digitsInRange :: [[Maybe Int]] -> Bool
+    digitsInRange = allTrue . map helper 
+      where
+        helper :: [Maybe Int] -> Bool
+        helper = allTrue . map (\x -> x `elem` Nothing:[Just x | x <- [1..9]]) 
+
+--A3
+isSolved :: Sudoku -> Bool
+isSolved (Sudoku rs) = foldl (\acc curr -> acc && not (Nothing `elem` curr)) True rs  
+
+--B1
+printSudoku :: Sudoku -> IO ()
+printSudoku (Sudoku rs) = (putStr . stringifySudoku) rs
+
+stringifySudoku :: [[Maybe Int]]  -> String
+stringifySudoku = foldl (\acc curr -> acc ++ printRow curr) "" 
+
+printRow :: [Maybe Int] -> String
+printRow row = reverse $ '\n' : (foldl (\acc curr -> (printCell curr):acc) "" row)
+
+printCell :: Maybe Int -> Char
+printCell Nothing = '.'
+printCell (Just x)= intToDigit x
+
+--B2
+-- readSudoku file reads from the file, and either delivers it, or stops
+-- if the file did not contain a sudoku
+readSudoku :: FilePath -> IO Sudoku
+readSudoku file = do
+  f <- readFile file
+  return (parseSudoku f)
+
+parseSudoku :: String -> Sudoku
+parseSudoku = (Sudoku . reverse . foldl (\acc curr -> (lineToRow curr):acc) [] . lines)
+
+lineToRow :: String -> [Maybe Int]
+lineToRow = reverse . foldl (\acc curr -> (charToCell curr):acc) []  
+
+charToCell :: Char -> Maybe Int
+charToCell '.' = Nothing
+charToCell c = Just (digitToInt c)
