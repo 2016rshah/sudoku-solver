465a071899dc90ea73940d7e4da4ac87895397b4
Rushi Shah <2016rshah@gmail.com>
2015-07-12 14:41:35 -0400

    Moved around code and fixed paths

--- src/Main.hs
+++ src/Main.hs
@@ -4,16 +4,23 @@ import Sudoku
 import Data.Maybe
 
 main = do
-  --examples
-  mediums
+  examples
+  --mediums
   --hards
+
+readAndSolve :: FilePath -> IO ()
+readAndSolve s = do
+  sud <- readSudoku s
+  printSudoku (fromJust (solve sud))
 
 examples :: IO ()
 examples  = do
-  putStrLn "\nExample solve. Should return solved board"
+  putStrLn "\nExample solve from hard coded board"
+  printSudoku (fromJust (solve example))
+  putStrLn "\nExample solve reading from file"
   readAndSolve "src/boards/examples/example.sud"
   putStrLn "\nImpossible board to solve"
-  readAndSolve "src/examples/impossible.sud"
+  readAndSolve "src/boards/examples/impossible.sud"
 
 mediums :: IO ()
 mediums = do 
@@ -29,7 +36,16 @@ hards = do
   putStrLn "\nHard example"
   readAndSolve "src/boards/hards/ex2.sud"
 
-readAndSolve :: FilePath -> IO ()
-readAndSolve s = do
-  sud <- readSudoku s
-  printSudoku (fromJust (solve sud))
+example :: Sudoku
+example =
+  Sudoku
+  [ [Just 3, Just 6, Nothing,Nothing,Just 7, Just 1, Just 2, Nothing,Nothing]
+  , [Nothing,Just 5, Nothing,Nothing,Nothing,Nothing,Just 1, Just 8, Nothing]
+  , [Nothing,Nothing,Just 9, Just 2, Nothing,Just 4, Just 7, Nothing,Nothing]
+  , [Nothing,Nothing,Nothing,Nothing,Just 1, Just 3, Nothing,Just 2, Just 8]
+  , [Just 4, Nothing,Nothing,Just 5, Nothing,Just 2, Nothing,Nothing,Just 9]
+  , [Just 2, Just 7, Nothing,Just 4, Just 6, Nothing,Nothing,Nothing,Nothing]
+  , [Nothing,Nothing,Just 5, Just 3, Nothing,Just 8, Just 9, Nothing,Nothing]
+  , [Nothing,Just 8, Just 3, Nothing,Nothing,Nothing,Nothing,Just 6, Nothing]
+  , [Nothing,Nothing,Just 7, Just 6, Just 9, Nothing,Nothing,Just 4, Just 3]
+  ]
--- src/Sudoku.hs
+++ src/Sudoku.hs
@@ -9,20 +9,6 @@ data Sudoku = Sudoku [[Maybe Int]]
 rows :: Sudoku -> [[Maybe Int]]
 rows (Sudoku rs) = rs
 
-example :: Sudoku
-example =
-  Sudoku
-  [ [Just 3, Just 6, Nothing,Nothing,Just 7, Just 1, Just 2, Nothing,Nothing]
-  , [Nothing,Just 5, Nothing,Nothing,Nothing,Nothing,Just 1, Just 8, Nothing]
-  , [Nothing,Nothing,Just 9, Just 2, Nothing,Just 4, Just 7, Nothing,Nothing]
-  , [Nothing,Nothing,Nothing,Nothing,Just 1, Just 3, Nothing,Just 2, Just 8]
-  , [Just 4, Nothing,Nothing,Just 5, Nothing,Just 2, Nothing,Nothing,Just 9]
-  , [Just 2, Just 7, Nothing,Just 4, Just 6, Nothing,Nothing,Nothing,Nothing]
-  , [Nothing,Nothing,Just 5, Just 3, Nothing,Just 8, Just 9, Nothing,Nothing]
-  , [Nothing,Just 8, Just 3, Nothing,Nothing,Nothing,Nothing,Just 6, Nothing]
-  , [Nothing,Nothing,Just 7, Just 6, Just 9, Nothing,Nothing,Just 4, Just 3]
-  ]
-
 --A1
 allBlankSudoku :: Sudoku
 allBlankSudoku = Sudoku (take 9 (repeat row))
